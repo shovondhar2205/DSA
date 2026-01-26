9820acf9171f6d1387ba2a70d5083615d7656565
shovondhar2205 <shovondhar138@gmail.com>
2026-01-26 13:16:32 +0530

    Quite same as the before one

---
+++ Array/09_Best_time_to_buy_and_sell_stock_II.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+};
+int main(){
+    Solution s;
+    vector<int> arr = {7,1,5,3,6,4};
+    int res = s.maxProfit(arr);
+    cout << res << endl;
+    return 0;
+}
