1e7fd098b8d461a1c4dc84206b5fe845a0885906
shovondhar2205 <shovondhar138@gmail.com>
2026-03-29 18:42:23 +0530

    Solved the problem using recursion

---
+++ Recursion/01_Fibonacci_Number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        } 
+        return fib(n-1) + fib(n-2);
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.fib(6) << endl;
+    return 0;
+}
+
+
