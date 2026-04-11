3a5f9d05d94ed450f2850b3a297d8a87cedb98ff
shovondhar2205 <shovondhar138@gmail.com>
2026-04-11 19:32:48 +0530

    Recursively taking the decisions to add or subtract the values

---
+++ Recursion/19_Target_Sum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int find(vector<int> &nums, int index, int n, int target){
+        // Base Case
+        if(index == n){
+            return target == 0;
+        }
+
+        // Recursion
+        return find(nums, index+1, n ,target-nums[index]) + find(nums, index+1, n, target+nums[index]);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return find(nums, 0, nums.size(), target);
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> arr = {1,1,1,1,1};
+    cout << s.findTargetSumWays(arr, 3) << endl;
+    return 0;
+}
