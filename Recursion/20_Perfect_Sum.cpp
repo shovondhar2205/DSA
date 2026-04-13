3162ea26e31de4a7440b4b6f8d81b74066ed3d80
shovondhar2205 <shovondhar138@gmail.com>
2026-04-13 09:31:54 +0530

    Recursively finding the count of possible answers

--- Recursion/20_Perfect_Sum.cpp
+++ Recursion/20_Perfect_Sum.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    
+    int find(vector<int> nums, int index, int n, int target){
+        // Base Case
+        if(target == 0){
+            return 1;
+        }
+        if(index == n || target < 0){
+            return 0;
+        }
+
+        // Recursion
+        return find(nums, index+1, n ,target) + find(nums, index+1, n, target-nums[index]);
+    }
 };
 
 int main(){
