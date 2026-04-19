0fc339f86551180e99034c763d8e600ba8b0cf0a
shovondhar2205 <shovondhar138@gmail.com>
2026-04-19 12:41:34 +0530

    Returning the maximum possible value

--- Recursion/30_Integer_Break.cpp
+++ Recursion/30_Integer_Break.cpp
@@ -4,16 +4,20 @@ using namespace std;
 class Solution {
 public:
     int help(int n, int index){
+        // Base case
         if(n == 0) return 1;
         if(n < 0 || index == 0) return 0;
 
+        // Skipping the element
         int skip = help(n, index-1);
 
+        // Taking the element
         int take = 0;
         if(n-index >= 0){
             take = index * help(n-index, index);
         }
         
+        // Returning the maximum possible value
         return max(skip, take);
     }
     
